282108a8051b5de50a6dec3bcff21ac00c87dddd
Pokachalova Viktoria <poka4alovavika@gmail.com>
2025-10-01 19:01:18 +0700

    Create first app

---
+++ 2025_10_01_first_app/ConsoleApplication10.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int w, n;
+    cin >> n >> w;
+    vector<int> a(n + 1);
+    vector<vector<bool>> dp(n + 1, vector<bool>(w + 1, false));
+    for (int i = 1;i < n + 1;++i) {
+        cin >> a[i];
+    }
+    dp[0][0] = true;
+    int ans = 0;
+    sort(a.begin(), a.end());
+    for (int i = 1;i < n + 1;++i) {
+        for (int j = 0;j < w + 1;++j) {
+            if (dp[i - 1][j] == true) {
+                dp[i][j] = true;
+            }
+            if (a[i] <= j && dp[i - 1][j - a[i]] == true) {
+                dp[i][j] = true;
+            }
+            if (dp[i][j] == true) {
+                ans = max(ans, j);
+
+            }
+        }
+    }
+    cout << ans;
+
+
+}
---
+++ 2025_10_01_first_app/build_run.bat
@@ -0,0 +1,14 @@
+@echo off
+chcp 1251 > log
+del log
+
+set MAIN=ConsoleApplication10.cpp
+set EXE=example.exe
+
+set CHARSET="-finput-charset=utf-8 -fexec-charset=windows-1251"
+
+if exist %EXE% del %EXE%
+
+g++ "%CHARSET%" %MAIN% -o %EXE%
+
+%EXE%
